24877820b651b123e209e6a8c245bfddd94de481
rahul4code <you@example.com>
2024-02-05 20:53:37 +0530

    socket server configs completed

---
+++ server.ts
@@ -0,0 +1,33 @@
+import { Server, Socket } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+const activeRoom = new Set();
+
+io.on("connection", (socket: Socket) => {
+  socket.emit("when-connected", "Socket is Online");
+
+  socket.on("join-room", (roomId) => {
+    console.log(roomId, "This is from client");
+    socket.join(roomId);
+  });
+
+  console.log(socket?.rooms, "Total Rooms");
+
+  socket.on("send-chat-message", ({ message, roomId }) => {
+    console.log(message, roomId, "getting Message");
+    socket
+      .to(roomId)
+      .emit("receive-message", { room: roomId, message: message });
+  });
+
+  socket.on("send-broadcast-message", (message) => {
+    socket.broadcast.emit("broadcast-message", message);
+  });
+});
+
+io.listen(5000);
